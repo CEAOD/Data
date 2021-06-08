b98e287c3596f4255ffdddd57cae6e27063ff391
Nikita Bhagam <bha.nikita1@gmail.com>
2021-06-08 18:43:04 -0400

    new commit

--- GH_Tomato/GH_Tomato_LESA/file_sort.py
+++ GH_Tomato/GH_Tomato_LESA/file_sort.py
@@ -17,7 +17,27 @@ name folder of capture as
 
 '''
 
-data_folder = "./data"
+valid_types = ['RPI']
+
+def camera_type(name):
+    spl = name.split("_")
+    spl = str(spl[1])
+    return spl.strip()
+
+
+input_directory = input("What is the filepath of your new data folder?\n")
+input_name = input("What is the name of your plant? Example: Tomato_RPI_1 is the first Tomato you are recording.\n")
+
+while camera_type(input_name) not in valid_types:
+    print('Type the input name properly! Plantname_camera_1, for example. Valid types are currently [\'RPI\']\n')
+    input_name = input("What is the name of your plant? Example: Tomato_RPI_1 is the first Tomato you are recording.\n")
+
+while not os.path.exists(input_directory):
+    print('Is that a valid directory??\n')
+    input_directory = input("What is the filepath of your new data folder?\n")
+
+
+
 folder_names = ["python_files","top_down_images","side_view_images","txt_files"]
 
 
@@ -25,20 +45,6 @@ top_dir = './top_down_images/'
 side_dir = './side_view_images/'
 py_dir = './python_files/'
 txt_dir = './txt_files/'
-
-
-#Finds side of camera angle
-def split_side(name_of_image_file):
-    spl = name_of_image_file.split("_")
-    tos = spl[4]
-    return tos
-
-#Splits time of image
-def split_time(name_of_image_file):
-    spl = name_of_image_file.replace("_ContinuousSide.jpg","")
-    spl = spl.replace("_ContinuousTop.jpg","")
-    return spl
-
 
 def folder(name):
     if not os.path.exists('./'+name):
@@ -48,9 +54,8 @@ def folder(name):
 
 #This sorts raspberri pi data into top and side directories
 #Takes filename and new folder name as input
-def sort_capture_rpi(filename, name):
-    img_dir = './data/' + filename + '/'
-    img_files = os.listdir(img_dir)
+def sort_capture_rpi(filepath, name):
+    img_files = os.listdir(filepath)
 
     if not os.path.exists('./top_down_images/'+name):
         os.makedirs('./top_down_images/'+name)
@@ -60,52 +65,88 @@ def sort_capture_rpi(filename, name):
         os.makedirs('./side_view_images/'+name)
         print ('Created:', name)
 
-    for j in range(int(len(img_files)/2)):
-        str1 = img_files[j*2]
-        str2 = img_files[j*2 + 1]
-        if not os.path.exists('./top_down_images/'+name+'/' +str(img_files[j*2])):
-            shutil.move(img_dir +  str(img_files[j*2]),'./top_down_images/'+name)
-        if not os.path.exists('./side_view_images/'+name+'/' +str(img_files[j*2+1])):
-            shutil.move(img_dir + str(img_files[j*2+1]),'./side_view_images/'+name)
+    if not os.path.exists('./redundant_images/'+name):
+        os.makedirs('./redundant_images/'+name)
+        print('Created: ', name)
 
-def camera_type(name):
-    spl = name.split("_")
-    spl = str(spl[1])
-    return spl.strip()
+    for j in range(int(len(img_files))):
+        str1 = img_files[j]
+        if('ContinuousTop' in str1):
+            if os.path.exists('./top_down_images/'+name):
+                if not os.path.exists('./top_down_images/'+name+ '/'+ str(img_files[j])):
+                    shutil.move(filepath + '/' + str(img_files[j]),'./top_down_images/'+name)
+        if('ContinuousSide' in str1):
+            if os.path.exists('./side_view_images/'+name):
+                if not os.path.exists('./side_view_images/'+name+ '/'+ str(img_files[j])):
+                    shutil.move(filepath + '/' + str(img_files[j]),'./side_view_images/'+name)
 
-
-
-
-
-
-#Only sorts text, python, and capture data where it goes top -> side in order
-def sort(current):
+'''
+def time_sort(current):
     files = os.listdir(current)
 
+    for i= 1 to len(files)
+        if len(files) > 1:
+                mid = len(files) // 2  # Finding the mid of the array
+                left = files[:mid]  # Dividing the array elements
+                right = files[mid:]  # into 2 halves
+
+                time_sort(left)
+                time_sort(right)
+
+                i = j = k = 0
+
+                #  data to temp arrays L[] and R[]
+                while i < len(left) and j < len(right):
+                    if left[i] < right[j]:
+                        time_sort[k] = left[i]
+                        i += 1
+                    else:
+                        time_sort[k] = right[j]
+                        j += 1
+                    k += 1
+
+                # Checking if any element was left
+                while i < len(left):
+                    unsorted_array[k] = left[i]
+                    i += 1
+                    k += 1
+
+                while j < len(right):
+                    unsorted_array[k] = right[j]
+                    j += 1
+                    k += 1
+
+'''
+
+def sort(current, name_of_plant):
+    files = os.listdir(current)
+    src = input_directory
+
+    for file in files:
+        print("Sorting..."+str(file) + '\n')
     for name in folder_names:
         folder(name)
 
     for file in os.listdir(current):
+        print(str(file))
         if file.endswith('.txt'):
             dest = './txt_files'
-            src = './data/'+ str(file)
             shutil.move(src,dest)
         if file.endswith('.py'):
-            src = './data/'+ str(file)
-            dest = 'python_files'
+            dest = './python_files'
             shutil.move(src,dest)
-        if os.path.isdir('./data/'+file):
-            cam_type = str(camera_type(str(file)))
+        if os.path.isdir(src + '/'+str(file)):
+            cam_type = str(camera_type(name_of_plant))
             if cam_type == 'RPI':
-                sort_capture_rpi(str(file),str(file))
-    files = os.listdir('./data/')
+                sort_capture_rpi(src + '/'+str(file), name_of_plant)
+
 
 def clean(current):
     for file in os.scandir(current):
         os.rmdir(file.path)
 
-sort(data_folder)
 
+sort(input_directory, input_name)
 print("Sorting the files...")
 
 
